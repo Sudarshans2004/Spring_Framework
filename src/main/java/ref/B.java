ce6c5c841379b3f20b556bf16af91422d8f0bdb4
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-07-14 12:22:27 +0530

    spring

---
+++ src/main/java/ref/B.java
@@ -0,0 +1,16 @@
+package ref;
+
+public class B {
+    private int y;
+
+    public B() {
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
