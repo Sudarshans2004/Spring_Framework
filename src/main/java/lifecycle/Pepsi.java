1bd03c71ed2d532834c7095c221f71aeb8388f9c
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-07-18 12:48:50 +0530

    spring

---
+++ src/main/java/lifecycle/Pepsi.java
@@ -0,0 +1,36 @@
+package lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class Pepsi implements InitializingBean, DisposableBean {
+    private double price;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Pepsi() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public String toString() {
+        return "Pepsi [price=" + price + "]";
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Pepsi bean initialized (afterPropertiesSet called)");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Pepsi bean destroyed (destroy called)");
+    }
+}
