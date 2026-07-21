1d7299b9cc087f06ee888251e74dc45850226547
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-07-21 12:33:24 +0530

    spring

---
+++ src/main/java/autowiring/annotation/Test.java
@@ -0,0 +1,12 @@
+package autowiring.annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("autowiring.annotation.xml");
+        Emp emp = (Emp) context.getBean("emp");
+        System.out.println(emp);
+    }
+}
